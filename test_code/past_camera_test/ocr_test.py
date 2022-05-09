56d930702ac14ea33db8a21ca5900f0ffb1532cd
Jacob Estep <jacobwestep@gmail.com>
2022-05-09 16:23:10 -0700

    re-wrote ocr test to work on pi, hopefully to diagnose issues in m2

--- test_code/past_camera_test/ocr_test.py
+++ test_code/past_camera_test/ocr_test.py
@@ -6,7 +6,7 @@ import crop_morphology
 from matplotlib import pyplot as plt
 import cv2
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+#pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 def ocr(image):
   return pytesseract.image_to_string(image)
@@ -36,25 +36,25 @@ def getWord(finger_x, finger_y):
 #p1 = load_image("photos with raspberry pi cam/test1.jpg")
 #p1 = load_image("photos with raspberry pi cam/img2_stand.jpg")
 
-in_path = 'photos with raspberry pi cam/test1.jpg'
-out_path = 'images_crop/2.jpg'
+in_path = 'test_code/past_camera_test/photos/photos with raspberry pi cam/test1.jpg'
+out_path = 'test_code/past_camera_test/photos/images_crop/2.jpg'
 #output = crop_morphology.process_image(path=in_path, out_path=out_path)
-output = cv2.imread(in_path)
+output1 = cv2.imread(filename=in_path)
 
-output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-output = cv2.threshold(output, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+output2 = cv2.cvtColor(output1, cv2.COLOR_BGR2GRAY)
+output3 = cv2.threshold(output2, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 #output.save(out_path)
 #print(ocr(output))
 #print(pytesseract.image_to_string(image=output, config='--oem 1'))
 
-details = pytesseract.image_to_data(output, output_type=pytesseract.Output.DICT)
+details = pytesseract.image_to_data(output3, output_type=pytesseract.Output.DICT)
 total_boxes = len(details['text'])
 
 for sequence_number in range(total_boxes):
   if float(details['conf'][sequence_number]) >30:
     (x, y, w, h) = (details['left'][sequence_number], details['top'][sequence_number], details['width'][sequence_number],  details['height'][sequence_number])
-    threshold_img = cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    threshold_img = cv2.rectangle(output2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 cv2.imshow('captured text', threshold_img)
 cv2.waitKey(0)
