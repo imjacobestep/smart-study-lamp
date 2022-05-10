6e7ebe6f79586069105f7f151d9fc1aa652bb2b2
Jacob Estep <jacobwestep@gmail.com>
2022-05-10 16:18:06 -0700

    added new ocr test using picamera

--- main_code/main_test.py
+++ main_code/main_test.py
@@ -7,6 +7,9 @@ import RPi.GPIO as GPIO
 import cv2
 from sympy import false, true
 import mediapipe as mp
+from picamera import PiCamera
+from io import BytesIO
+from PIL import Image
 #import lamp_main
 
 mp_drawing = mp.solutions.drawing_utils
@@ -17,6 +20,12 @@ y_pos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 
 brightnesses = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 colors = [0, 1, 2]
+
+camera = PiCamera()
+camera.resolution (2592, 1944)
+camera.framerate = 15
+camera.use_video_port = True
+stream = BytesIO()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(utilities.pin_table["auto switch"], GPIO.IN, pull_up_down=GPIO.PUD_UP) #auto switch
@@ -94,6 +103,42 @@ def test_tracking():
                 break
     cap.release()
 
+def test_tracking2():
+    camera.start_preview()
+    with mp_hands.Hands(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+            while True:
+                camera.capture(stream, format='jpeg')
+                stream.seek(0)
+                image = Image.open(stream)
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = hands.process(image)
+
+                # Draw the hand annotations on the image.
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                image_height, image_width, _ = image.shape
+                if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        x = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width
+                        y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height
+                        print("Index fingertip at: " + str(x) + ", " + str(y))
+                        #track_movement(x, y)
+                        camera_service.track_movement(image, x,y)
+                        mp_drawing.draw_landmarks(
+                            image,
+                            hand_landmarks,
+                            mp_hands.HAND_CONNECTIONS,
+                            mp_drawing_styles.get_default_hand_landmarks_style(),
+                            mp_drawing_styles.get_default_hand_connections_style())
+                # Flip the image horizontally for a selfie-view display.
+                #cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+    camera.stop_preview()
+
 while True:
     userIn = input("what do you want to test? adj, env, cam, or auto: ")
     if userIn == "adj":
@@ -101,7 +146,7 @@ while True:
     if userIn == "env":
         test_learning_env()
     if userIn == "cam":
-        test_tracking()
+        test_tracking2()
     if userIn == "auto":
         test_auto()
     if userIn == "exit":
