28aa408d6146027ef5e87db2590f1675bde072c0
Jacob Estep <jacobwestep@gmail.com>
2022-05-30 13:40:42 -0700

    added working db_test.py file

---
+++ test_code/db_test.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import random
+
+## CLOUD FIRESTORE SETUP ##
+cred = credentials.Certificate("smart-study-lamp-firebase-adminsdk-pgq1y-eeb5253a08.json")
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def test(word):
+    wordID = random.randint(1,2000000)
+    print("uploading word...")
+    word_doc = db.collection('words').document(str(wordID))
+    word_doc.set({
+        'word':word,
+        'learned': False
+    })
+    print("finished")
+
+test(input("Input a word to send to the database: "))
